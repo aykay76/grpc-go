4b13f66908748d0fbe8c1ca24282d88edf5d2e34
Alan Kelly <alan.kelly.london@gmail.com>
2020-04-14 20:44:37 +0100

    observe durations

--- server.go
+++ server.go
@@ -18,6 +18,11 @@ var (
 		Name: "grpc_server_operations_total",
 		Help: "The total number of processed operations",
 	})
+
+	opsDuration = promauto.NewSummary(prometheus.SummaryOpts{
+		Name: "grpc_server_operations_duration",
+		Help: "The duration of processed operations",
+	})
 )
 
 // EnvironmentServer : server for the environment service
@@ -27,17 +32,28 @@ type EnvironmentServer struct {
 
 // GetEnvironmentVariable : allow clients to get specified environment variable
 func (server *EnvironmentServer) GetEnvironmentVariable(ctx context.Context, kvp *pb.KeyValuePair) (*pb.KeyValuePair, error) {
+	// create a timer to observe the duration
+	timer := prometheus.NewTimer(opsDuration)
+
+	// increment the counter of total operations
 	opsProcessed.Inc()
 
 	var result pb.KeyValuePair
 	result.Key = kvp.Key
 	result.Value = os.Getenv(kvp.Key)
 
+	// observe the time since the timer was started
+	timer.ObserveDuration()
+
 	return &result, nil
 }
 
 // GetEnvironmentVariables : allows clients to get all environment variables on a stream
 func (server *EnvironmentServer) GetEnvironmentVariables(req *empty.Empty, stream pb.EnvironmentService_GetEnvironmentVariablesServer) error {
+	// create a timer to observe the duration
+	timer := prometheus.NewTimer(opsDuration)
+
+	// increment the counter of total operations
 	opsProcessed.Inc()
 
 	for _, e := range os.Environ() {
@@ -53,13 +69,22 @@ func (server *EnvironmentServer) GetEnvironmentVariables(req *empty.Empty, strea
 		}
 	}
 
+	timer.ObserveDuration()
+
 	return nil
 }
 
 // SetEnvironmentVariable : allows clients to set environment variables
 func (server *EnvironmentServer) SetEnvironmentVariable(ctx context.Context, kvp *pb.KeyValuePair) (*empty.Empty, error) {
+	// create a timer to observe the duration
+	timer := prometheus.NewTimer(opsDuration)
+
+	// increment the counter of total operations
 	opsProcessed.Inc()
 
 	os.Setenv(kvp.Key, kvp.Value)
+
+	timer.ObserveDuration()
+
 	return nil, nil
 }
